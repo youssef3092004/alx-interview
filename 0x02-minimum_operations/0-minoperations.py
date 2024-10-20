dba6c49e0a975a29b4bed91c58a2aecb4a089fb7
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-20 19:40:14 +0300

    Update minOperations function to comply with PEP 8 standards.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-""" 
-  Minimum Operations
-"""
+"""Minimum Operations"""
+
 def minOperations(n):
-  """
-  Calculate the minimum number of operations needed to achieve exactly n 'H' characters in a text file,
-  starting with a single 'H'. The only operations allowed are 'Copy All' and 'Paste'.
+    """
+    Calculate the minimum number of operations needed to achieve exactly n 'H' characters in a text file,
+    starting with a single 'H'. The only operations allowed are 'Copy All' and 'Paste'.
 
-  Args:
-    n (int): The target number of 'H' characters. Must be a positive integer.
-  Returns:
-    int: The minimum number of operations required to reach exactly n 'H' characters.
-  Returns 0 if n is less than or equal to 1, as no operations are needed.
-  """
-  if n <= 1:
-    return 0
+    Args:
+        n (int): The target number of 'H' characters. Must be a positive integer.
 
-  operation = 0
-  i = 2
+    Returns:
+        int: The minimum number of operations required to reach exactly n 'H' characters.
+             Returns 0 if n is less than or equal to 1, as no operations are needed.
+    """
+    if n <= 1:
+        return 0
 
-  while n > 1:
-    if n % i == 0:
-      operation += i
-      n //= i
-    else:
-      i += 1
-  return operation
+    operation = 0
+    i = 2
+
+    while n > 1:
+        if n % i == 0:
+            operation += i
+            n //= i
+        else:
+            i += 1
+    return operation
